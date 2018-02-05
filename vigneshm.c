43d51586217747f4e32d8fa5d693e627f6d03caa
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-02-05 16:23:56 +0530

    vigneshm.c

---
+++ vigneshm.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int x;
+printf("enter the year");
+scanf("%d",&x);
+if((x%4)==0)
+printf("it is leap year");
+else 
+printf("\n it is not a leap year");
+return 0;
+}
