7daa07519d1cdee307a5fd976d28c5ae91d7d221
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-02-05 16:58:46 +0530

    Create vignesh11.c

---
+++ vignesh11.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,results;
+printf("\n enter the value of a:");
+scanf("%d",&a);
+printf("\n enter the value of b:");
+scanf("%d",&b);
+printf("\n power value is:");
+result=power (a,b0;
+printf("%d",results);
+return 0;
+}
