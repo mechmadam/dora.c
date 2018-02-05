3e838602ee9e61bc7ef910cbe4811cde72754033
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-02-05 16:35:46 +0530

    vigdora.c

---
+++ vigdora.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+viod main()
+{
+int n,i,sum=0;
+printf("enter the positive value");
+scanf("%d",&n);
+for (i=a;i<=n;i++)
+{
+sum+=i;
+}
+printf("sum=%d",sum);
+}
