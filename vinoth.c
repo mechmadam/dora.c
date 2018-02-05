90081e9005d0e1a9bf05c4deea11c3a71bef1a14
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-02-05 16:44:14 +0530

    vinoth.c

---
+++ vinoth.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,sum,n,k,a[5];
+printf("Enter the two number :\n");
+scanf("%d%d",&n,&k);
+printf("enter the values :");
+for(i=o;i<n;i++)
+scanf("%d",&a[i]);
+}
+sum=k+a[0];
+printf("the sum is %d",sum);
+return 0;
+}
